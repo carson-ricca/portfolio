f3d7aa7d63bc0f585a55e0eef20a79dd2c874b75
Carson Ricca <59884796+carson-ricca@users.noreply.github.com>
2022-08-15 16:09:35 -0400

    Initial Website Version (#1)

---
+++ constants/data.ts
@@ -0,0 +1,101 @@
+const userData = {
+  githubUsername: "carson-ricca",
+  name: "Carson Ricca",
+  designation: "Full-Stack Developer",
+  avatarUrl: "/avatar2.png",
+  email: "carsonricca28@gmail.com",
+  address: "Toronto, ON",
+  projects: [
+    {
+      title: "Canvas Gamification",
+      link: "https://github.com/canvas-gamification",
+      imgUrl: "/gamification.png",
+    },
+    {
+      title: "Postn",
+      link: "https://postn-dev.herokuapp.com/",
+      imgUrl: "/postn.png",
+    },
+    {
+      title: "Thesis",
+      link: "https://github.com/carson-ricca/thesis",
+      imgUrl: "/thesis.png",
+    },
+    {
+      title: "Smelly Code",
+      link: "https://github.com/carson-ricca/smelly-code",
+      imgUrl: "/smelly-code.png",
+    },
+    {
+      title: "Portfolio",
+      link: "https://github.com/carson-ricca/oomck-bot",
+      imgUrl: "/algochurn.png",
+    },
+  ],
+  about: {
+    title:
+      "I'm a software developer that loves building products and web applications that impact people around the globe",
+    description: [
+      "I have 2 years of experience in Software Development/Web Development. I love building products from the ground up working across the entire stack and I'm passionate about improving the user experience.",
+      "Currently, I'm a software developer at IBM within the Consulting practice. My current focus is in the Contact Center as a Service (CCaaS) domain, focusing on building custom contact center solutions to meet specific client needs.",
+      "My studies and practical experiences have provided me with proficient knowledge in software development processes, methods, and techniques by exposing me to various technical stacks. ",
+      "Technologies: Angular, React, Python, Django, TypeScript/JavaScript, Java, Spring, and SQL.",
+      "CSS Frameworks: Bootstrap, Taiga UI, Material UI, Tailwind CSS.",
+      "Project Management Tools: Jira, Trello, Confluence, GitHub, Bitbucket.",
+    ],
+    currentJob: "IBM",
+    currentJobUrl: "https://www.ibm.com/ca-en?lnk=m",
+  },
+  experience: [
+    {
+      title: "Software Developer",
+      company: "IBM",
+      year: "2022",
+      companyLink: "https://www.ibm.com/ca-en?lnk=m",
+      desc: "Currently focused in the Contact Centre as a Service (CCaas) domain building custom contact centre solutions to meet specific client needs.",
+    },
+    {
+      title: "Graduated University",
+      company: "University of British Columbia",
+      year: "2022",
+      companyLink: "https://www.ubc.ca/",
+      desc: "Graduated from UBC with a BSc in Computer Science. Graduated with honours for the completion of a thesis. Focused on implementing a Dynamic Bayesian Network to analyze a student's past performance at solving programming questions and make suggestions on the difficulty of question the student should tackle next in order to optimize their learning.",
+    },
+    {
+      title: "Research Assistant (Web Development)",
+      company: "University of British Columbia",
+      year: "2021",
+      companyLink: "https://www.ubc.ca/",
+      desc: "Focused on building a platform for first-year computer science students to gain more practical experience with programming through gamification. Developed across the entire stack with a focus on Angular and Django. Was responsible for reviewing data collected from the software and utilized this analysis to build on the project roadmap and integrate it with the course taught at the university.",
+    },
+    {
+      title: "Web Developer",
+      company: "Government of Canada",
+      year: "2021",
+      companyLink: "https://www.canada.ca/en.html",
+      desc: "Joined a different team within the same organization. Focused on frontend development of a web application. The UI was developed in TypeScript using the React framework. Refactored and improved existing code, as well as implementing new features at the request of the client. Conducted functional testing over the course of the web development lifecycle. Was involved in demoing new features directly to the client.",
+    },
+    {
+      title: "Full Stack Web Developer",
+      company: "Government of Canada",
+      year: "2020",
+      companyLink: "https://www.canada.ca/en.html",
+      desc: "Developed microservices and web applications using Java, TypeScript, Angular, and SQL. Regularly contributed ideas and suggestions in team meetings. Deliverd updates on deadlines, designs, and enhacements. Conducted unit testing to deliver optimal browser functionality.",
+    },
+    {
+      title: "Graduated High School",
+      company: "St. Andrew's College",
+      year: "2018",
+      companyLink: "https://www.sac.on.ca/",
+      desc: "Focused on computer science and computer engineering topics. Played on multiple varsity sports, as well as a member of the programming club.",
+    },
+  ],
+  resumeUrl:
+    "https://drive.google.com/file/d/1vQgFhPf6xWPjQoQxrEpwhoVQwPuTBT7g/view?usp=sharing",
+  socialLinks: {
+    linkedin: "https://www.linkedin.com/in/carson-ricca",
+    github: "https://github.com/carson-ricca",
+  },
+};
+
+export default userData;
---
+++ pages/api/contact.ts
@@ -0,0 +1,53 @@
+import nodemailer from "nodemailer";
+import { google } from "googleapis";
+
+const CLIENT_ID = process.env.clientId;
+const CLIENT_SECRET = process.env.clientSecret;
+const REDIRECT_URI = process.env.redirectURI;
+const REFRESH_TOKEN = process.env.refreshToken;
+const TO_EMAIL = process.env.toEmail;
+const FROM_EMAIL = process.env.fromEmail;
+
+const oAuth2Client = new google.auth.OAuth2(
+  CLIENT_ID,
+  CLIENT_SECRET,
+  REDIRECT_URI
+);
+oAuth2Client.setCredentials({ refresh_token: REFRESH_TOKEN });
+
+export default async function handler(req, res) {
+  try {
+    const accessToken = await oAuth2Client.getAccessToken();
+    const transporter = nodemailer.createTransport({
+      host: "smtp.gmail.com",
+      port: 465,
+      secure: true,
+      auth: {
+        type: "OAuth2",
+        user: "carsonriccaweb@gmail.com",
+        clientId: CLIENT_ID,
+        clientSecret: CLIENT_SECRET,
+        refreshToken: REFRESH_TOKEN,
+        accessToken: accessToken.token,
+      },
+    });
+
+    const mailData = {
+      from: FROM_EMAIL,
+      to: TO_EMAIL,
+      subject: `Portfolio Question from ${req.body.name}`,
+      text: `Email: ${req.body.email}\nMessage: ${req.body.message}`,
+    };
+
+    transporter.sendMail(mailData, (err, info) => {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log(info);
+      }
+    });
+    return res.status(200).json({ message: "Mail is sent." });
+  } catch (error) {
+    return res.status(500).end({ error: error });
+  }
+}
