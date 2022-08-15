3d1b31c2150f790ab6e6786660debabb3c604a34
Carson Ricca <59884796+carson-ricca@users.noreply.github.com>
2022-08-15 18:48:50 -0400

    Fix Sharing Bug (#4)

--- components/ContainerBlock.tsx
+++ components/ContainerBlock.tsx
@@ -27,22 +27,17 @@ export default function ContainerBlock({ children, ...customMeta }: Props) {
         <meta content={meta.description} name="description" />
         <meta
           property="og:url"
-          content={`https://yourwebsite.com${router.asPath}`}
+          content={`https://carsonricca.com${router.asPath}`}
         />
         <link
           rel="canonical"
-          href={`https://yourwebsite.com${router.asPath}`}
+          href={`https://carsonricca.com${router.asPath}`}
         />
         <meta property="og:type" content={meta.type} />
         <meta property="og:site_name" content="Carson Ricca" />
         <meta property="og:description" content={meta.description} />
         <meta property="og:title" content={meta.title} />
         <meta property="og:image" content={meta.image} />
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:site" content="@mannupaaji" />
-        <meta name="twitter:title" content={meta.title} />
-        <meta name="twitter:description" content={meta.description} />
-        <meta name="twitter:image" content={meta.image} />
         {/* {meta.date && (
           <meta property="article:published_time" content={meta.date} />
         )} */}
